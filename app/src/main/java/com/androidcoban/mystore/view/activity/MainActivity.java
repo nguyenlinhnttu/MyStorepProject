118531e0cf266ab3f4ca5cc8e902835d36ac6c08
Linh <nguyenlinh.nttu@gmail.com>
2017-04-09 19:14:54 +0700

    Initial commit

---
+++ app/src/main/java/com/androidcoban/mystore/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.androidcoban.mystore.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androidcoban.mystore.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
